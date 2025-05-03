8fc6c20d58e042527ae3c32916c81443bae535d1
Riyas532 <suriya6543256@gmail.com>
2025-05-03 08:27:23 +0530

    Add files via upload

---
+++ recommendation.py
@@ -0,0 +1,5 @@
+def get_recommendations(query):
+    return [
+        {"name": "Numerical Reasoning", "url": "https://example.com/numerical", "remote": "Yes", "adaptive": "Yes", "duration": "20 min", "type": "Cognitive"},
+        {"name": "Verbal Reasoning", "url": "https://example.com/verbal", "remote": "Yes", "adaptive": "No", "duration": "18 min", "type": "Cognitive"}
+    ]
